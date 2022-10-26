9499e26f4e0f12c1d1d28e3410b0d64647af9969
Meghea Iulian <iulian.meghea@gmail.com>
2022-10-26 14:28:10 +0300

    chore(deps): upgrade

--- flake.nix
+++ flake.nix
@@ -6,14 +6,27 @@
     core.inputs.home-manager.url = "github:nix-community/home-manager";
     nix-vim.url = "github:megheaiulian/nix-vim";
     nix-vim.inputs.nixpkgs.follows = "nixpkgs";
+    nur.url = github:nix-community/NUR;
   };
 
-  outputs = { self, nixpkgs, core, nix-vim, ... }: {
+  outputs = { self, nixpkgs, core, nix-vim, nur, ... }: {
     nixosConfigurations.vision = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = (builtins.attrValues core.nixosModules) ++ [
         ./configuration.nix
-        ({ ... }: { nix.registry.nixpkgs.flake = nixpkgs; })
+        ({ ... }: {
+          nix.registry.nixpkgs.flake = nixpkgs;
+          nixpkgs.overlays = [
+            (final: prev: ({
+              nur = import nur {
+                nurpkgs = prev;
+                pkgs = prev;
+              };
+            }))
+          ];
+        })
+
+
         {
           home-manager.users.iulian = { ... }: {
             imports = (builtins.attrValues (nixpkgs.lib.filterAttrs (n: v: n != "lf") core.homeModules)) ++ [ ./home.nix ];
